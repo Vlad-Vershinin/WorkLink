00d94d119800226c6d8bcf1ea63fb50d27359dd1
Vlad-Vershinin <vladversinin676@gmail.com>
2025-11-15 22:28:31 +0500

    add input component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Button from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { Mail } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="">
-      <Button>Какая то длинная кнопка</Button>
+
     </div>
   );
 }
